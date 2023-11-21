a424cdf282c12aa1eb190873de9efa9e8fe09429
Juan <jd.rodriguez.or@gmail.com>
2023-11-21 18:21:46 -0500

    created react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      {/* Your components and content here */}
+    </div>
+  );
+}
+
+export default App;
